afa8e48a9ce98633f3e98315f528b219e76d73db
flowinginthewind700 <yongtaofu@gmail.com>
2025-01-11 17:40:48 +0800

    restore bg

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <WavyBackground className="futuristic-bg-light dark:futuristic-bg-dark tech-pattern min-h-screen">
+            <div className="futuristic-bg-light dark:futuristic-bg-dark tech-pattern min-h-screen">
               {children}
               <Footer />
-            </WavyBackground>
+            </div>
         </ThemeProvider>
         <Analytics />
         <BottomNavbar />
