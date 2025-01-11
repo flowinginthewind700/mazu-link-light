e642dcb9cf361942e3d55a875aee61f2af27c9ee
flowinginthewind700 <yongtaofu@gmail.com>
2025-01-11 17:35:45 +0800

    change background

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { Footer } from '@/components/footer'
 import dynamic from 'next/dynamic'
 import { BottomNavbar } from '@/components/bottom-navbar'
+import { WavyBackground } from "@/components/ui/wavy-background";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,10 +25,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="futuristic-bg-light dark:futuristic-bg-dark tech-pattern min-h-screen">
+            <WavyBackground className="futuristic-bg-light dark:futuristic-bg-dark tech-pattern min-h-screen">
               {children}
               <Footer />
-            </div>
+            </WavyBackground>
         </ThemeProvider>
         <Analytics />
         <BottomNavbar />
