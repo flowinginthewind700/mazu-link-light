dd946a87ee86d31b28f8c5ab88d9fa6f4a4a707f
flowinginthewind700 <605577690@qq.com>
2025-02-07 06:47:07 +0000

    fix sitemap and ads

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,31 +4,44 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-      config.resolve.alias['@'] = path.resolve(__dirname, './src');
-      return config;
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    return config;
   },
   images: {
     remotePatterns: [
-     {
-         protocol: 'https',
-         hostname: 'sdimage-1253436389.cos.ap-nanjing.myqcloud.com',
-         port: '',
-         pathname: '/**',
-       },
-     {
-         protocol: 'https',
-          hostname: 'cms.llmstock.com',
-         port: '',
-         pathname: '/**',
-       },
-     ],
-   },
-   async redirects() {
+      {
+        protocol: 'https',
+        hostname: 'sdimage-1253436389.cos.ap-nanjing.myqcloud.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cms.llmstock.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
     return [
       {
         source: '/sitemap.xml',
         destination: '/api/sitemap.xml',
-        permanent: true,
+      },
+    ];
+  },
+  // 新增这个配置
+  async headers() {
+    return [
+      {
+        source: '/(sitemap.xml|ads.txt|robots.txt)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
       },
     ];
   },
