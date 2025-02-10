5f88f5e229d32e577c6150e8ccf01c4b661b704a
flowinginthewind700 <605577690@qq.com>
2025-02-10 13:47:20 +0000

    fix ads

--- frontend/src/components/ads/GoogleAdSense.tsx
+++ frontend/src/components/ads/GoogleAdSense.tsx
@@ -1,21 +1,21 @@
 // components/ads/GoogleAdSense.tsx
 'use client';
-import Script from 'next/script';
+import { useEffect } from 'react';
 
 export const GoogleAdSense = () => {
   const adsenseClientId = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID;
 
-  if (!adsenseClientId) {
-    console.warn('AdSense client ID is not set in environment variables.');
-    return null;
-  }
+  useEffect(() => {
+    if (adsenseClientId) {
+      const script = document.createElement('script');
+      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClientId}`;
+      script.async = true;
+      script.crossOrigin = 'anonymous';
+      document.head.appendChild(script);
+    } else {
+      console.warn('AdSense client ID is not set in environment variables.');
+    }
+  }, [adsenseClientId]);
 
-  return (
-    <Script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClientId}`}
-      strategy="afterInteractive"
-      crossOrigin="anonymous"
-    />
-  );
+  return null;
 };
