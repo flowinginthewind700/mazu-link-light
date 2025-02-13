1c0cd6e1dc6de239b4a7a6fa984515bdf458025c
flowinginthewind700 <605577690@qq.com>
2025-02-13 11:22:25 +0000

    remove div

--- frontend/src/components/halftoneProcessor.tsx
+++ frontend/src/components/halftoneProcessor.tsx
@@ -485,7 +485,7 @@ const HalftoneProcessor: React.FC = () => {
   }, [processFrame])
 
   return (
-    <div className="p-0 max-w-4xl mx-auto">
+    // <div className="p-0 max-w-4xl mx-auto">
       <Card className="p-4">
         <div
           className="border-2 border-dashed border-gray-300 rounded-lg p-6 mb-6 text-center cursor-pointer"
@@ -598,7 +598,7 @@ const HalftoneProcessor: React.FC = () => {
           <Button onClick={saveResult}>Save Result</Button>
         </div>
       </Card>
-    </div>
+    {/* </div> */}
   )
 }
 
