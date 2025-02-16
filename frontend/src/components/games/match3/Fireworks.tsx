3b586ff0cebccace172e014eb6e2a808c6b3cd29
flowinginthewind700 <yongtaofu@gmail.com>
2025-02-16 09:14:14 +0800

    change fireworks

--- frontend/src/components/games/match3/Fireworks.tsx
+++ frontend/src/components/games/match3/Fireworks.tsx
@@ -17,6 +17,7 @@ const Fireworks: React.FC = () => {
     canvas.height = window.innerHeight
 
     const particles: Particle[] = []
+    const fireworks: Firework[] = []
 
     class Particle {
       x: number
@@ -25,23 +26,30 @@ const Fireworks: React.FC = () => {
       speedX: number
       speedY: number
       color: string
+      alpha: number
 
-      constructor(canvasWidth: number, canvasHeight: number) {
-        this.x = Math.random() * canvasWidth
-        this.y = canvasHeight
-        this.size = Math.random() * 2 + 1
-        this.speedX = Math.random() * 6 - 3
-        this.speedY = Math.random() * -15
-        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`
+      constructor(x: number, y: number, color: string) {
+        this.x = x
+        this.y = y
+        this.size = Math.random() * 3 + 2
+        const angle = Math.random() * Math.PI * 2
+        const speed = Math.random() * 3 + 2
+        this.speedX = Math.cos(angle) * speed
+        this.speedY = Math.sin(angle) * speed
+        this.color = color
+        this.alpha = 1
       }
 
       update() {
         this.x += this.speedX
         this.y += this.speedY
+        this.speedY += 0.05 // gravity
+        this.alpha -= 0.01
         if (this.size > 0.1) this.size -= 0.1
       }
 
       draw(ctx: CanvasRenderingContext2D) {
+        ctx.globalAlpha = this.alpha
         ctx.fillStyle = this.color
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
@@ -49,30 +57,66 @@ const Fireworks: React.FC = () => {
       }
     }
 
-    function createParticles() {
-      if (!canvas) return // Ensure canvas is not null
-      for (let i = 0; i < 50; i++) {
-        particles.push(new Particle(canvas.width, canvas.height))
+    class Firework {
+      x: number
+      y: number
+      color: string
+      particles: Particle[]
+
+      constructor(canvasWidth: number, canvasHeight: number) {
+        this.x = Math.random() * canvasWidth
+        this.y = Math.random() * canvasHeight * 0.5
+        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`
+        this.particles = []
+
+        for (let i = 0; i < 200; i++) {
+          this.particles.push(new Particle(this.x, this.y, this.color))
+        }
+      }
+
+      update() {
+        this.particles.forEach((particle, index) => {
+          particle.update()
+          if (particle.alpha <= 0) {
+            this.particles.splice(index, 1)
+          }
+        })
+      }
+
+      draw(ctx: CanvasRenderingContext2D) {
+        this.particles.forEach((particle) => particle.draw(ctx))
       }
     }
 
-    function animateParticles() {
-      if (!canvas || !ctx) return // Ensure canvas and ctx are not null
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      particles.forEach((particle, index) => {
-        particle.update()
-        particle.draw(ctx)
-        if (particle.size <= 0.1) {
-          particles.splice(index, 1)
-        }
-      })
-      requestAnimationFrame(animateParticles)
+    function createFirework() {
+      if (!canvas) return
+      fireworks.push(new Firework(canvas.width, canvas.height))
     }
 
-    createParticles()
-    animateParticles()
+    function animateFireworks() {
+      if (!canvas || !ctx) return
+      ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    const interval = setInterval(createParticles, 1000)
+      fireworks.forEach((firework, index) => {
+        firework.update()
+        firework.draw(ctx)
+        if (firework.particles.length === 0) {
+          fireworks.splice(index, 1)
+        }
+      })
+
+      requestAnimationFrame(animateFireworks)
+    }
+
+    // Create initial fireworks
+    for (let i = 0; i < 5; i++) {
+      createFirework()
+    }
+
+    animateFireworks()
+
+    const interval = setInterval(createFirework, 1000)
 
     return () => {
       clearInterval(interval)
@@ -83,3 +127,4 @@ const Fireworks: React.FC = () => {
 }
 
 export default Fireworks
+
